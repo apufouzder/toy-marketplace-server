359be176ca657623207729447d8089bb1885968a
apufouzder <apufouzdar@gmail.com>
2023-06-09 12:17:25 -0700

    Complete

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fpfcy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -93,8 +88,6 @@ async function run() {
 
 
 
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -107,10 +100,8 @@ run().catch(console.dir);
 
 
 
-
-
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello server");
 });
 
 app.listen(port, () => {
